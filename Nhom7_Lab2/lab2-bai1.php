6b3c66fc8a3073512b894bb3106e9cf8279a8a5e
lengockimyen261107kyy <lengockimyen261107kyy@gmail.com>
2026-05-18 13:42:40 +0700

    lab2/kimyen

---
+++ Nhom7_Lab2/lab2-bai1.php
@@ -0,0 +1,41 @@
+<?php
+class Person{
+    public $name;
+    public $age;
+    public $address;
+
+    public function setName($name){
+        $this->name = $name;
+    }
+    public function setAge($age){
+        $this->age = $age;
+    }   
+    public function setAddress($address){
+        $this->address = $address;
+    }
+    public function getInfo(){
+        return "Name: " . $this ->name ."<br>" . 
+               "Age: " . $this ->age ."<br>" . 
+               "Address: " . $this ->address;
+    }
+    public function canVote(){
+        if($this->age >= 18){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+    $person = new Person();
+    $person ->setName("chosDung");
+    $person ->setAge(100);
+    $person ->setAddress("ag St");
+
+    echo $person ->getInfo() . "<br>";
+    if($person ->canVote()){
+        echo "This person can vote.";
+    } else {
+        echo "This person cannot vote.";
+    }
+
+

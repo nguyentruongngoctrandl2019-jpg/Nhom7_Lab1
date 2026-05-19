2703f6054904e7a3e586aa58cd02b58a7ebfe0e6
TUONGVY <tuongvyphan0211travinh@gmail.com>
2026-05-19 11:12:29 +0700

    [Lab02]TuongVy

---
+++ Lab02/bai2.php
@@ -0,0 +1,45 @@
+<?php
+class Product {
+    public $name;
+    public $price;
+    public $quantity;
+
+    // Set tên sản phẩm
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    // Set giá
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+
+    // Set số lượng
+    public function setQuantity($quantity) {
+        $this->quantity = $quantity;
+    }
+
+    // Hiển thị thông tin sản phẩm
+    public function getInfo() {
+        return "Product Name: " . $this->name . "<br>" .
+               "Price: $" . $this->price . "<br>" .
+               "Quantity: " . $this->quantity;
+    }
+
+    // Tính tổng giá trị sản phẩm
+    public function calculateTotal() {
+        return $this->price * $this->quantity;
+    }
+}
+
+// Sử dụng lớp Product
+$product = new Product();
+
+$product->setName("IPhone X");
+$product->setPrice(999);
+$product->setQuantity(10);
+
+// Hiển thị thông tin sản phẩm và tính tổng giá trị
+echo $product->getInfo() . "<br>";
+echo "Total: $" . $product->calculateTotal();
+?>
